5c5bb923f93fdf62615b1f8e4118e50868b1ec76
Zino2201 <zineddineamazouz@gmail.com>
2022-05-04 23:23:05 +0200

    hack

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -75,9 +75,9 @@ resume_site_dir() {
 setup_gh() {
   if [[ -z $(git branch -av | grep "$PAGES_BRANCH") ]]; then
     _no_pages_branch=true
-    git checkout -b "$PAGES_BRANCH"
+    git checkout -b "$PAGES_BRANCH" --force
   else
-    git checkout "$PAGES_BRANCH"
+    git checkout "$PAGES_BRANCH" --force
   fi
 }
 
